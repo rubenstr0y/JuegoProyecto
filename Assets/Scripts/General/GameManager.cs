c7dcfdbb12a369bbc57cf01d116fbc1c5f967afa
rubenstr0y <rubenk17mm@gmail.com>
2026-03-25 14:23:47 +0100

    Enemy + GameManager Basics
    Enemy AI + Basic Game Manager (cross referencer)

--- Assets/Scripts/General/GameManager.cs
+++ Assets/Scripts/General/GameManager.cs
@@ -1,10 +1,28 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
 
     [SerializeField] private BruteEnemy testEnemy;
 
-    private Hole[] Holes;
+    [SerializeField] private Hole[] Holes;
+
+
+    public List<Hole> SearchAvaliableHoles()
+    {
+
+        Hole[] CurrentHoles = Holes;
+        List<Hole> AvaliableHoles = new List<Hole>();
+
+        foreach (Hole hole in CurrentHoles)
+        {
+            if ((!hole.is_hole_occupied) && (hole.is_hole_active))
+            {
+                AvaliableHoles.Add(hole);
+            }
+        }
+        return AvaliableHoles;
+    }
 
 }
